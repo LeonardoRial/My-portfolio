32fa86401eb7f1a8241b187e2e84e1efc36419bd
LeonardoCamposRial <73833933+LeonardoCamposRial@users.noreply.github.com>
2022-08-11 10:52:07 +0100

    "Observer.js fix"

--- src/js/observer.js
+++ src/js/observer.js
@@ -8,6 +8,7 @@ const homeObserver = new IntersectionObserver(function (entries, observer) {
   entries.forEach((entry) => {
     entry.target.classList.add("slide-in");
     if (!entry.isIntersecting) {
+      return;
     }
     observer.unobserve(entry.target);
   });
